ba5328bfba610c5fa314af133e19110c4a51d4e6
srscalzo1 <142814646+srscalzo1@users.noreply.github.com>
2025-03-06 20:26:06 -0500

    added initial files to play with

---
+++ bluemain.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5 import QtWidgets
+from bluesen import Ui_MainWindow  # Import your generated UI class
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  # Create the application object
+    MainWindow = QtWidgets.QMainWindow()  # Create the main window
+    ui = Ui_MainWindow()  # Instantiate the UI class
+    ui.setupUi(MainWindow)  # Set up the UI on the main window
+    MainWindow.show()  # Show the main window
+    sys.exit(app.exec_())  # Start the application event loop
+
+if __name__ == "__main__":
+    main()
