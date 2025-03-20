193f8db644975347a685085a4920764a39d7636e
Ahmed Tawfig <ahmed.h.t03@gmail.com>
2025-03-20 00:53:29 -0400

    Add UI initialization and refactored updateMap to take numerical inputs only.

---
+++ app_logic.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+import folium
+import io
+
+from PyQt5.QtWidgets import QMainWindow
+
+from ece4805_graphics.bluesen_v3 import Ui_MainWindow
+
+
+class AppWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  # Load UI from bluesen_v3.py
+        self.pushButton.clicked.connect(self.updateMap)  # Override method
+
+    def updateMap(self):
+        try:
+            coordinate = (float(self.lineEdit.text()), float(self.lineEdit_2.text()))
+            geoMap = folium.Map(zoom_start=11, location=coordinate)
+            data = io.BytesIO()
+            geoMap.save(data, close_file=False)
+            self.webView.setHtml(data.getvalue().decode())
+        except ValueError:
+            print("Invalid input: Please enter numeric values for coordinates.")
